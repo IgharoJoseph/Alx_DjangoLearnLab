e7f440b9593c6a301ff57c53747a1ab22e989945
IgharoJoseph <josephigharo@gmail.com>
2025-11-27 16:35:54 +0100

    Implementing Custom Permissions in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -19,4 +19,8 @@ urlpatterns = [
     path('admin-dashboard/', admin_view, name='admin_view'),
     path('librarian-dashboard/', librarian_view, name='librarian_view'),
     path('member-dashboard/', member_view, name='member_view'),
+
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/edit/', views.edit_book, name='edit_book'),
+    path('books/delete/', views.delete_book, name='delete_book'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import user_passes_test, permission_required
+from .models import Book, Author
+from .forms import BookForm
 
 # --- Registration view ---
 def register(request):
@@ -38,3 +40,30 @@ def is_member(user):
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
+
+
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list_books')
+    else:
+        form = BookForm()
+    return render(request, 'relationship_app/book_form.html', {'form': form})
+
+# Add Book view
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    return render(request, 'relationship_app/add_book.html')  # minimal template
+
+# Edit Book view
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request):
+    return render(request, 'relationship_app/edit_book.html')
+
+# Delete Book view
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request):
+    return render(request, 'relationship_app/delete_book.html')
