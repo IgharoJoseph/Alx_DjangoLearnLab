5986d3fe79076f3f7c822b63e9d9d119d25a2b5b
IgharoJoseph <josephigharo@gmail.com>
2025-11-27 15:59:12 +0100

    Implement Role-Based Access Control in Django correction 2

--- django-models/LibraryProject/relationship_app/templates/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/templates/relationship_app/admin_view.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseForbidden
 
-def is_admin(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
-
-@user_passes_test(is_admin)
+@login_required
 def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
+    # Must check role = "Admin"
+    if not hasattr(request.user, "role") or request.user.role != "Admin":
+        return HttpResponseForbidden("You are not authorized to view this page.")
+
+    # No dashboard needed — plain text response is okay
+    return HttpResponse("Welcome to the Admin Page")
