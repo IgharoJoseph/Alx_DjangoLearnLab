69e1fd705eb47942cfde962271e53824a251d620
IgharoJoseph <josephigharo@gmail.com>
2025-11-27 20:15:26 +0100

    Implementing a Custom User Model in Django 1

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import  User, AbstractUser, BaseUserManager
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 from django.db import models
@@ -10,3 +12,34 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.author}"
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required for all users.")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if not extra_fields["is_staff"] or not extra_fields["is_superuser"]:
+            raise ValueError("Superuser must have is_staff=True and is_superuser=True.")
+
+        return self.create_user(username, email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to="profile/", null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
