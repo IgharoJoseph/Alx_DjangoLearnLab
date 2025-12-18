6240c6a3fba16ddd6b2a3ed199dd4bd96b04b0db
IgharoJoseph <josephigharo@gmail.com>
2025-12-18 11:59:27 +0100

    Initial Django project with models, views, and URLs corrections

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,26 +1,8 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from . import views
-from .admin_view import admin_view
-from .librarian_view import librarian_view
-from .member_view import member_view
+from .views import list_books
+from .views import LibraryDetailView
 
 urlpatterns = [
-    # Example existing views
-    path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-
-    # Authentication
-    path('register/', views.register, name='register'),
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-
-    # Role-based dashboards
-    path('admin-dashboard/', admin_view, name='admin_view'),
-    path('librarian-dashboard/', librarian_view, name='librarian_view'),
-    path('member-dashboard/', member_view, name='member_view'),
-
-    path('add_book/', views.add_book, name='add_book'),
-    path('edit_book/', views.edit_book, name='edit_book'),
-    path('delete_book/', views.delete_book, name='delete_book'),
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
